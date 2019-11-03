4166ba0712ca329f9757cef5625ccbca576ce967
Uzair Ashraf <uzinatorcl@live.com>
2019-11-03 14:55:05 -0800

    still setting up, going to do wireframing before going any further:

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Route } from 'react-router-dom';
+import HomePage from './homepage';
+import AppContext from '../lib/context';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
   render() {
     return (
-      null
+      <div className="min-vh-100 d-flex pt-5">
+        <Route path ="/" Component={HomePage}/>
+      </div>
     );
   }
 }
---
+++ client/components/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HomePage(props) {
+  return (
+    <div className="landing-page">
+      <div className="heading">
+        grader.
+      </div>
+      <div className="subheading">
+
+      </div>
+    </div>
+  );
+}
