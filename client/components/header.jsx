f4d56407c6b8d376e00af151b404b7313bdc3868
Uzair Ashraf <uzinatorcl@live.com>
2019-11-04 21:09:29 -0800

    logo is now showing

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import AppContext from '../lib/context';
+import { Drawer } from '@material-ui/core';
 
 export default class Header extends React.Component {
   render() {
-    return (
-      <header className="navbar fixed-top navbar-light bg-light border-bottom py-0 justify-content-center">
+    if (!this.context.user) {
+      return (
+        <header className="navbar fixed-top navbar-light bg-light border-bottom py-0 justify-content-center navigation">
         grader.
-      </header>
-    );
+        </header>
+      );
+    } else {
+      return (
+        <header className="navbar fixed-top navbar-light bg-light border-bottom py-0 justify-content-center navigation">
+          grader.
+          <div className="appDrawer">
+            <i className="fas fa-bars" />
+          </div>
+        </header>
+      );
+    }
   }
 }
+
+Header.contextType = AppContext;
